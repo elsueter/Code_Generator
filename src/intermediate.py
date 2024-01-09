2bc433d69edd99a8cbd8a7d0e4b4f62fe30b9ba1
Joseph Straw <joseph_straw@outlook.com>
2024-01-09 19:36:55 +0000

    updated C intermediary reprisentation

--- src/intermediate.py
+++ src/intermediate.py
@@ -44,24 +44,36 @@
 #   <statement>
 #}
 
-# digit          : 0-9
-# digit_list     : <digit_list> <digit> | <digit>
-# letter         : a-z | A-Z
-# otherChar      : |!”%&/()=+-*#><;,^.][\n \t
-# character      : <digit> | <letter> | <otherChar>
-# character_list : <character_list> <character> | <character>
+# digit             : 0-9
+# digit_list        : <digit_list> <digit> | <digit>
+# letter            : a-z | A-Z
+# other_char        : |!”%&/()=+-*#><;,^.][\n \t
+# character         : <digit> | <letter> | <otherChar>
+# character_list    : <character_list> <character> | <character>
+# sign              : + | - | <none>
 #
-# int            : <sign> <digit_list>
-# char           : <character_list>
-# float          : <sign> <digit_list> . <digit_list> f
+# type_specifier    : int
+#                   | char
+#                   | float
+#                   | void
 #
-# type           : <int>
-#                | <char>
-#                | <float>
-#                | <double>
+# int               : <sign> <digit_list>
+# char              : <character_list>
+# float             : <sign> <digit_list> . <digit_list> f
 #
-# typeSpec       : int
-#                | char
-#                | float
-#                | double
-#                | void
+# type              : <int>
+#                   | <char>
+#                   | <float>
+#
+# definition        : <type_specificer> <type>
+#
+# stmt              : <definition>
+# stmt_list         : <stmt_list> <stmt> | <stmt>
+#
+# exp               :
+# exp_list          : <exp_list> <exp> | <exp>
+#
+# brackets          : ( <exp_list> | <stmt_list> )
+# braces            : { <exp_list> | <stmt_list> }
+#
+# if                : if <brackets <exp_list> > <braces <stmt_list> >
